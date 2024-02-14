e2484bd880452597c528a2141fd18591137a6cb1
Andrew Gearhart <agearh@gmail.com>
2024-02-14 08:51:18 -0600

    fix typo in requirements in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="pequena-arana",
     version="1.0",
     packages=find_packages(),
-    install_requires=["networks", "pytest", "npyscreen"],
+    install_requires=["networkx", "pytest", "npyscreen"],
     license="MIT",
     long_description=open("README.md").read(),
 )
